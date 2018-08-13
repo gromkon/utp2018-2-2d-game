2828aa7be2a8ef559306a2f5b081920e72782608
Anna Petrova <34690633+mghnsm@users.noreply.github.com>
2018-08-13 18:45:33 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -5,39 +5,87 @@ let objectCollision = {
 };
 
 let objectTypes = {
-  1 : {
-    name : "Tree",
-    sp : objectTree,
-    sp2: null,
-    sprite : [{x:0, y:0, w:80, h:100}],
-    sprite2: null,
-    offset : [-20, -60],
-    offset2: null,
-    collision : objectCollision.solid,
-    zIndex : 3
-  },
-  2 : {
-    name : "Tall Grass",
-    sp : tallGrass,
-    sp2: tallGrass2,
-    sprite : [{x:0, y:0, w:40, h:50}],
-    sprite2: [{x:0, y:0, w:40, h:50}],
-    offset : [0, -10],
-    offset2: [0, -10],
-    collision : objectCollision.none,
-    zIndex : 2
-  },
-  3 : {
-    name : "Stone",
-    sp : stone,
-    sp2: null,
-    sprite : [{x:0, y:0, w:40, h:44}],
-    sprite2: null,
-    offset : [0,-4],
-    offset2: null,
-    collision : objectCollision.solid,
-    zIndex : 2
-  }
+    
+    1 : {
+      name : "Tree",
+      sp : objectTree,
+      sprite : [{x:0, y:0, w:80, h:100}],
+      offset : [-20, -60],
+      collision : objectCollision.solid,
+      zIndex : 3
+    },
+    
+    2 : {
+      name : "Tall Grass",
+      sp : objectGrass,
+      sprite : [{x:0, y:0, w:40, h:50}],
+      offset : [0, -10],
+      collision : objectCollision.none,
+      zIndex : 2
+    },
+    
+    3 : {
+      name : "Stone",
+      sp : objectStone,
+      sprite : [{x:0, y:0, w:40, h:44}],
+      offset : [0, -4],
+      collision : objectCollision.solid,
+      zIndex : 2
+    },
+    
+    4 : {
+      name : "Roof",
+      sp : objectRoof,
+      sprite : [{x:0, y:0, w:240, h:80}],
+      offset : [0, 0],
+      collision : objectCollision.solid,
+      zIndex : 2
+    },
+
+    5 : {
+      name: "Roof 1",
+      sp : objectRoof1,
+      sprite : [{x:0, y:0, w:520, h:280}],
+      offset : [0, 0],
+      collision: objectCollision.solid,
+      zIndex : 3
+    },
+
+    6 : {
+      name: "Roof 2",
+      sp : objectRoof2,
+      sprite : [{x:0, y:0, w:520, h:280}],
+      offset : [0, 0],
+      collision: objectCollision.solid,
+      zIndex : 3
+    },
+        
+    7 : {
+        name : "Roof 3",
+        sp : objectRoof3,
+        sprite : [{x:0, y:0, w:280, h:40}],
+        offset : [0, 0],
+        collision : objectCollision.solid,
+        zIndex : 3
+    },
+    
+    8 : {
+        name : "Roof 3 1",
+        sp : objectRoof31,
+        sprite : [{x:0, y:0, w:200, h:40}],
+        offset : [0, 0],
+        collision : objectCollision.solid,
+        zIndex : 3
+    },
+    
+    9 : {
+      name : "Fountain",
+      sp : objectFountain,
+      sprite : [{x:0, y:0, w:200, h:200}],
+      offset : [0, 10],
+      collision : objectCollision.solid,
+      zIndex : 1
+    }
 }
 
 function MapObject(a) {
