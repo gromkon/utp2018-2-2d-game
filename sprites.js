9ff258246f32aa8e445434ce1663d13b4db0a484
Anna Petrova <34690633+mghnsm@users.noreply.github.com>
2018-08-13 18:46:01 +0300

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -1,5 +1,6 @@
 let water = new Image(),
     path = new Image(),
+    asphalt = new Image(),
     grass = new Image(),
     wall = new Image(),
     tree = new Image(),
@@ -7,14 +8,25 @@ let water = new Image(),
     ice = new Image(),
     sand = new Image(),
     bush = new Image(),
+    stairs = new Image(),
+    
     objectTree = new Image(),
-    stone = new Image(),
-    tallGrass = new Image(),
-    tallGrass2 = new Image(),
+    objectStone = new Image(),
+    objectGrass = new Image(),
+    
+    objectRoof = new Image(),
+    objectRoof1 = new Image(),
+    objectRoof2 = new Image(),
+    objectRoof3 = new Image(),
+    objectRoof31 = new Image(),
+    
+    objectFountain = new Image(),
+    
     hero = new Image();
 
 water.src = 'sprites/water.jpg';
 path.src = 'sprites/path.jpg';
+asphalt.src = 'sprites/asphalt.png';
 grass.src = 'sprites/grass.jpg';
 wall.src = 'sprites/wall.jpg';
 tree.src = 'sprites/tree.jpg';
@@ -22,8 +34,18 @@ snow.src = 'sprites/snow.jpg';
 ice.src = 'sprites/ice.jpg';
 sand.src = 'sprites/sand.jpg';
 bush.src = 'sprites/bush.jpg';
-hero.src = 'sprites/hero.png';
-tallGrass.src = 'sprites/grass.png';
-tallGrass2.src = 'sprites/grass2.png';
-stone.src = 'sprites/stone.png';
+stairs.src = 'sprites/stairs.png';
+
 objectTree.src = 'sprites/tree.png';
+objectStone.src = 'sprites/stone.png';
+objectGrass.src = 'sprites/grass.png';
+
+objectRoof.src = 'sprites/roof.png';
+objectRoof1.src = 'sprites/roof1.png';
+objectRoof2.src = 'sprites/roof2.png';
+objectRoof3.src = 'sprites/roof3.png';
+objectRoof31.src = 'sprites/roof31.png';
+
+objectFountain.src = 'sprites/fountain.png';
+
+hero.src = 'sprites/hero.png';
