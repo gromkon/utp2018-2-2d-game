0d1e98775a8f2167ea6ffe16151e90a8cdbef878
Anna Petrova <34690633+mghnsm@users.noreply.github.com>
2018-08-13 18:47:49 +0300

    Add files via upload

---
+++ tileTypes.js
@@ -0,0 +1,14 @@
+let tileTypes = {
+    0: {floor:floorTypes.solid, sprite: wall}, //СЃС‚РµРЅР°
+    1: {floor:floorTypes.path, sprite: grass}, //С‚СЂР°РІР°
+    2: {floor:floorTypes.path, sprite: path}, //РґРѕСЂРѕРіР°
+    3: {floor:floorTypes.solid, sprite: tree}, //РґРµСЂРµРІСЊСЏ
+    4: {floor:floorTypes.water, sprite: water}, //РІРѕРґР°
+    5: {floor:floorTypes.path, sprite: bush}, //РєСѓСЃС‚С‹
+    6: {floor:floorTypes.path, sprite: snow}, //СЃРЅРµРі
+    7: {floor:floorTypes.path, sprite: sand}, //РїРµСЃРѕРє
+    8: {floor:floorTypes.ice, sprite: ice}, //Р»РµРґ
+    9: {floor:floorTypes.path, sprite: asphalt},
+    
+    10: {floor:floorTypes.path, sprite: stairs}
+};
