a7984aa7d467d71abbbdab204ebea745e1ec058c
Alexsandra <40834933+AlexsaTasara@users.noreply.github.com>
2018-08-17 23:41:11 +0300

    Добавлен инвентарь и остановка игрового времени.
    Что бы остановить время нужно нажать "s".
    Исправлены баги перемещения.
    Добавлена возможность создания предметов, которые персонаж может носить в инвентаре.

---
+++ inventory.js
@@ -0,0 +1,62 @@
+let itemTypes = {
+  1:{
+    name: null,
+    maxStack: null,
+    sprite: null,
+    offset: null
+  }
+};
+
+function Stack(id, qty) {
+  this.id = id;
+  this.qty = qty;
+}
+
+function Inventory(s) {
+  this.spaces = s;
+  this.stacks = [];
+}
+
+Inventory.prototype.addItems = function(id, qty) {
+  for(let i = 0; i < this.spaces; i++) {
+    if(this.stacks.length <= i){
+      let maxHere = (qty > itemTypes[id].maxStack ?
+      itemTypes[id].maxStack : qty);
+      this.stacks.push(new Stack(id, maxHere));
+      qty -= maxHere;
+    }
+    else{
+      if(this.stacks[i].type == id &&
+         this.stacks[i].aty < itemTypes[id].maxStack) {
+        let maxHere = (itemTypes[id].maxStack - this.stacks[i].qty);
+        if(maxHere > qty) {
+          maxHere = qty;
+        }
+        this.stacks[i].qty += maxHere;
+        qty -= maxHere;
+      }
+    }
+    if(qty == 0) {
+      return 0;
+    }
+  }
+  return qty;
+}
+
+function PlacedItemStack(id, qty) {
+  this.type = id;
+  this.qty = qty;
+  this.x = 0;
+  this.y = 0;
+  this.mapN = 0;
+}
+
+PlacedItemStack.prototype.placeAt = function(nx, ny, mapN) {
+  if(mapTileData[mapN].map[toIndex(this.x, this.y)].itemStack == this){
+    mapTileData[mapN].map[toIndex(this.x, this.y)].itemStack = null;
+  }
+  this.x = nx;
+  this.y = ny;
+  this.mapN = mapN;
+  mapTileData[mapN].map[toIndex(nx, ny)].itemStack = this;
+};
