bbcefe4d8043e075529898580e2b418a9770fe62
gromkon <38605972+gromkon@users.noreply.github.com>
2018-08-23 20:10:12 +0300

    Изменен стиль кода

--- characterMain.js
+++ characterMain.js
@@ -1,15 +1,35 @@
 function Character() {
-  this.tileFrom = [1, 1];
-  this.tileTo = [1, 1];
-  this.timeMoved = 0;
-  this.dimensions = [30, 30];
-  this.position = [45, 45];
-  this.delayMove = 150;
-  this.direction = directions.down;
-  this.sprites = {};
-  this.sprites[directions.down] = [{x:4, y:20, w:20, h:30}];
-  this.sprites[directions.up] = [{x:4, y:320, w:20, h:30}];
-  this.sprites[directions.left] = [{x:4, y:120, w:18, h:30}];
-  this.sprites[directions.right] = [{x:4, y:220, w:18, h:30}];
-  this.inventory = new Inventory(10);
+	this.tileFrom = [1, 1];
+	this.tileTo = [1, 1];
+	this.timeMoved = 0;
+	this.dimensions = [30, 30];
+	this.position = [45, 45];
+	this.delayMove = 150;
+	this.direction = directions.down;
+	this.sprites = {};
+	this.sprites[directions.down] = [{
+		x: 4,
+		y: 20,
+		w: 20,
+		h: 30
+	}];
+	this.sprites[directions.up] = [{
+		x: 4,
+		y: 320,
+		w: 20,
+		h: 30
+	}];
+	this.sprites[directions.left] = [{
+		x: 4,
+		y: 120,
+		w: 18,
+		h: 30
+	}];
+	this.sprites[directions.right] = [{
+		x: 4,
+		y: 220,
+		w: 18,
+		h: 30
+	}];
+	this.inventory = new Inventory(10);
 }
