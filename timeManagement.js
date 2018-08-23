79c8cf2ccc99b9b73c6740da689176e269e1429e
gromkon <38605972+gromkon@users.noreply.github.com>
2018-08-23 20:09:42 +0300

    Изменен стиль кода, замена переменной
    Изменен стиль кода;
    Переменная gameSpeeds (строчка 2) теперь константа (раньше задавалась через let, теперь через const);

--- timeManagement.js
+++ timeManagement.js
@@ -1,6 +1,11 @@
 let gameTime = 0;
-let gameSpeeds = [
-  {name: "Normal", mult:1},
-  {name: "Paused", mult:0}
+const gameSpeeds = [{
+		name: "Normal",
+		mult: 1
+	},
+	{
+		name: "Paused",
+		mult: 0
+	}
 ];
 let currentSpeed = 0;
