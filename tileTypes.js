e11b6da34d74c8f08526263f54bf135ddbf172dc
gromkon <38605972+gromkon@users.noreply.github.com>
2018-08-24 20:31:19 +0300

    Изменен стиль кода
    Переменная tileTypes (строчка 1) теперь константа (раньше задавалась через let, теперь через const);
    Убраны коментарии написанные неподдерживаемым шрифтом;

--- tileTypes.js
+++ tileTypes.js
@@ -1,16 +1,50 @@
-let tileTypes = {
-    0: {floor:floorTypes.solid, sprite: wall}, //СЃС‚РµРЅР°
-    1: {floor:floorTypes.path, sprite: grass}, //С‚СЂР°РІР°
-    2: {floor:floorTypes.path, sprite: path}, //РґРѕСЂРѕРіР°
-    3: {floor:floorTypes.solid, sprite: tree}, //РґРµСЂРµРІСЊСЏ
-    4: {floor:floorTypes.water, sprite: water}, //РІРѕРґР°
-    5: {floor:floorTypes.path, sprite: bush}, //РєСѓСЃС‚С‹
-    6: {floor:floorTypes.path, sprite: snow}, //СЃРЅРµРі
-    7: {floor:floorTypes.path, sprite: sand}, //РїРµСЃРѕРє
-    8: {floor:floorTypes.ice, sprite: ice}, //Р»РµРґ
-    9: {floor:floorTypes.path, sprite: asphalt},
-    
-    10: {floor:floorTypes.path, sprite: stairs},
-    
-    11: {floor:floorTypes.solid, sprite: asphalt}
+const tileTypes = {
+	0: {
+		floor: floorTypes.solid,
+		sprite: wall
+	},
+	1: {
+		floor: floorTypes.path,
+		sprite: grass
+	},
+	2: {
+		floor: floorTypes.path,
+		sprite: path
+	},
+	3: {
+		floor: floorTypes.solid,
+		sprite: tree
+	},
+	4: {
+		floor: floorTypes.water,
+		sprite: water
+	},
+	5: {
+		floor: floorTypes.path,
+		sprite: bush
+	},
+	6: {
+		floor: floorTypes.path,
+		sprite: snow
+	},
+	7: {
+		floor: floorTypes.path,
+		sprite: sand
+	},
+	8: {
+		floor: floorTypes.ice,
+		sprite: ice
+	},
+	9: {
+		floor: floorTypes.path,
+		sprite: asphalt
+	},
+	10: {
+		floor: floorTypes.path,
+		sprite: stairs
+	},
+	11: {
+		floor: floorTypes.solid,
+		sprite: asphalt
+	}
 };
