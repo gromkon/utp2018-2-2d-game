4cea6acf46d25cce750265be9586d9116a1a667c
Alexsandra <40834933+AlexsaTasara@users.noreply.github.com>
2018-08-24 17:00:23 +0300

    Добавление системы прокачивания и параметров героя
    Работаю над системой параметров персонажа и геймплеем. Во время посещения "занятий" повышаются параметры и происходят события в зависимости от действий персонажа. События нужно добавлять в папку "events.js". Как устроено добавление событий описано внутри.
    Исправила один баг с перемещением и персонаж отрисовывается выше.

---
+++ dayManagment.js
@@ -0,0 +1,57 @@
+let dayNo = 0;
+let events = 0;
+let visit = {}
+visit[0] = false;
+visit[1] = false;
+visit[2] = false;
+visit[3] = false;
+visit[4] = false;
+visit[5] = false;
+visit[6] = false;
+
+function nextDay(){
+  dayNo++;
+  visit[0] = false;
+  visit[1] = false;
+  visit[2] = false;
+  visit[3] = false;
+  visit[4] = false;
+  visit[5] = false;
+  visit[6] = false;
+  events = 0;
+}
+
+function attendingLesson(a, b, c){
+  events++;
+  if(a!=6){
+    if(b==0){// Принял участие в уроке
+      player.subject[a].Points += 2;
+      player.stat[stats.intelegent].Points += 2;
+      player.subject[a].Understanding += player.stat[stats.intelegent].Lv + 1 + player.subject[a].bonus;
+      lOE[dayNo][a][b][c];
+    }
+    if(b==1){// Разговор с однокласниками.
+      player.subject[a].Points += 2;
+      player.stat[stats.charisma].Points += 2;
+    }
+    if(b==2){// Попытался сбежать с урока.
+      lOE[dayNo][a][b][c];
+    }
+  }
+  else{
+    if(b==0){// Принял участие в уроке
+      player.subject[a].Points += 2;
+      player.stat[stats.strength].Points += 2;
+      player.subject[a].Understanding += player.stat[stats.intelegent].Lv + 1 + player.subject[a].bonus;
+      lOE[dayNo][a][b][c];
+    }
+    if(b==1){// Разговор с однокласниками.
+      player.subject[a].Points += 2;
+      player.stat[stats.charisma].Points += 2;
+      lOE[dayNo][a][b][c];
+    }
+    if(b==2){// Попытался сбежать с урока.
+      lOE[dayNo][a][b][c];
+    }
+  }
+}
