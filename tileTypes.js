a74645c66af2b52e95b6a924a666bd634b0411d2
Anna Petrova <34690633+mghnsm@users.noreply.github.com>
2018-08-25 18:55:38 +0300

    Update tileTypes.js

--- tileTypes.js
+++ tileTypes.js
@@ -13,32 +13,33 @@ const tileTypes = {
 	10: { floor: floorTypes.path, sprite: stairs },
     
 	99: { floor: floorTypes.solid, sprite: asphalt },
-    98: { floor: floorTypes.path, sprite: clear },
-    97: { floor: floorTypes.solid, sprite: floor2 },
+	98: { floor: floorTypes.path, sprite: clear },
+	97: { floor: floorTypes.solid, sprite: floor2 },
     
-    11: { floor: floorTypes.path, sprite: floor1 },
-    12: { floor: floorTypes.path, sprite: floor2 },
-    13: { floor: floorTypes.path, sprite: floor3 },
-    31: { floor: floorTypes.path, sprite: floor31 },
-    32: { floor: floorTypes.path, sprite: floor32 },
-    14: { floor: floorTypes.path, sprite: floorHall },
+	11: { floor: floorTypes.path, sprite: floor1 },
+	12: { floor: floorTypes.path, sprite: floor2 },
+	13: { floor: floorTypes.path, sprite: floor3 },
+	31: { floor: floorTypes.path, sprite: floor31 },
+	32: { floor: floorTypes.path, sprite: floor32 },
+	14: { floor: floorTypes.path, sprite: floorHall },
     
-    15: { floor:floorTypes.solid, sprite: wall11 },
-    16: { floor:floorTypes.solid, sprite: wall12 },
-    17: { floor:floorTypes.solid, sprite: wall13 },
-    18: { floor:floorTypes.solid, sprite: wall14 },
-    
-    27: { floor:floorTypes.solid, sprite: wall15 },
-    28: { floor:floorTypes.solid, sprite: wall16 },
-    29: { floor:floorTypes.solid, sprite: wall17 },
-    30: { floor:floorTypes.solid, sprite: wall18 },
-    
-    19: { floor:floorTypes.solid, sprite: corner11 },
-    20: { floor:floorTypes.solid, sprite: corner12 },
-    21: { floor:floorTypes.solid, sprite: corner13 },
-    22: { floor:floorTypes.solid, sprite: corner14 },
-    23: { floor:floorTypes.solid, sprite: corner15 },
-    24: { floor:floorTypes.solid, sprite: corner16 },
-    25: { floor:floorTypes.solid, sprite: corner17 },
-    26: { floor:floorTypes.solid, sprite: corner18 }
+	15: { floor:floorTypes.solid, sprite: wall11 },
+	16: { floor:floorTypes.solid, sprite: wall12 },
+	17: { floor:floorTypes.solid, sprite: wall13 },
+	18: { floor:floorTypes.solid, sprite: wall14 },
+
+	27: { floor:floorTypes.solid, sprite: wall15 },
+	28: { floor:floorTypes.solid, sprite: wall16 },
+	29: { floor:floorTypes.solid, sprite: wall17 },
+	30: { floor:floorTypes.solid, sprite: wall18 },
+	
+	19: { floor:floorTypes.solid, sprite: corner11 },
+	20: { floor:floorTypes.solid, sprite: corner12 },
+	21: { floor:floorTypes.solid, sprite: corner13 },
+	22: { floor:floorTypes.solid, sprite: corner14 },
+	23: { floor:floorTypes.solid, sprite: corner15 },
+	24: { floor:floorTypes.solid, sprite: corner16 },
+	25: { floor:floorTypes.solid, sprite: corner17 },
+	26: { floor:floorTypes.solid, sprite: corner18 }
 };
+
