3803975965cb8553df65f50e8e00caf1e7a00297
Anna Petrova <34690633+mghnsm@users.noreply.github.com>
2018-08-25 15:45:30 +0300

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -2,13 +2,36 @@ let water = new Image(),
     path = new Image(),
     asphalt = new Image(),
     grass = new Image(),
-    wall = new Image(),
+    clear = new Image(),
     tree = new Image(),
     snow = new Image(),
     ice = new Image(),
     sand = new Image(),
     bush = new Image(),
     stairs = new Image(),
+    
+    floor1 = new Image(),
+    floor2 = new Image(),
+    floor3 = new Image(),
+    floorHall = new Image(),
+    
+    wall11 = new Image(),
+    wall12 = new Image(),
+    wall13 = new Image(),
+    wall14 = new Image(),
+    wall15 = new Image(),
+    wall16 = new Image(),
+    wall17 = new Image(),
+    wall18 = new Image(),
+    
+    corner11 = new Image(),
+    corner12 = new Image(),
+    corner13 = new Image(),
+    corner14 = new Image(),
+    corner15 = new Image(),
+    corner16 = new Image(),
+    corner17 = new Image(),
+    corner18 = new Image(),
     
     objectTree = new Image(),
     objectStone = new Image(),
@@ -28,13 +51,38 @@ water.src = 'sprites/water.jpg';
 path.src = 'sprites/path.jpg';
 asphalt.src = 'sprites/asphalt.png';
 grass.src = 'sprites/grass.jpg';
-wall.src = 'sprites/wall.jpg';
+clear.src = 'sprites/clear.png';
 tree.src = 'sprites/tree.jpg';
 snow.src = 'sprites/snow.jpg';
 ice.src = 'sprites/ice.jpg';
 sand.src = 'sprites/sand.jpg';
 bush.src = 'sprites/bush.jpg';
 stairs.src = 'sprites/stairs.png';
+
+floor1.src = 'sprites/floor1.png';
+floor2.src = 'sprites/floor2.png';
+floor3.src = 'sprites/floor3.png';
+floorHall.src = 'sprites/floor_hall.png';
+
+wall11.src = 'sprites/wall11.png';
+wall12.src = 'sprites/wall12.png';
+wall13.src = 'sprites/wall13.png';
+wall14.src = 'sprites/wall14.png';
+
+wall15.src = 'sprites/wall15.png';
+wall16.src = 'sprites/wall16.png';
+wall17.src = 'sprites/wall17.png';
+wall18.src = 'sprites/wall18.png';
+
+corner11.src = 'sprites/corner11.png';
+corner12.src = 'sprites/corner12.png';
+corner13.src = 'sprites/corner13.png';
+corner14.src = 'sprites/corner14.png';
+corner15.src = 'sprites/corner15.png';
+corner16.src = 'sprites/corner16.png';
+corner17.src = 'sprites/corner17.png';
+corner18.src = 'sprites/corner18.png';
+
 
 objectTree.src = 'sprites/tree.png';
 objectStone.src = 'sprites/stone.png';
