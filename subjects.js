9084d3464bc6e798d55c7eb9b264a09c5734b868
mghnsm <mghnsm@gmail.com>
2018-08-30 19:23:28 +0300

    Добавлено ограничение на посещение локаций, предметы вынесены в отдельный файл

---
+++ subjects.js
@@ -0,0 +1,28 @@
+const subjects = {
+	CS: 0, //Информатика
+	MA: 1, //Математический анализ
+	LA: 2, //Линейная алгебра
+	EN: 3, //Иностранные языки
+	SS: 4, //Социальные науки
+	PR: 5, //Языки и методы программирования
+}
+
+let subject = {};
+subject[subjects.CS] = {
+	Points: 0
+};
+subject[subjects.MA] = {
+	Points: 0
+};
+subject[subjects.LA] = {
+	Points: 0
+};
+subject[subjects.EN] = {
+	Points: 0
+};
+subject[subjects.SS] = {
+	Points: 0
+};
+subject[subjects.PR] = {
+	Points: 0
+};
