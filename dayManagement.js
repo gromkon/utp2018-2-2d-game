3a33c4327bda7ab425cdd66571803cd7acbdd269
mghnsm <mghnsm@gmail.com>
2018-08-30 22:16:52 +0300

    Внесены незначительные изменения

--- dayManagement.js
+++ dayManagement.js
@@ -18,21 +18,3 @@ function nextDay() {
 	visit[5] = false;
 	events = 0;
 }
-
-function attendingLesson(a, b, c) {
-	events++;
-	if (a != 6) {
-		if (b == 0) { // Принял участие в уроке
-			player.subject[a].Points += 2;
-			player.subject[a].Understanding += player.stat[stats.intelegent].Lv + 1 + player.subject[a].bonus;
-			lOE[dayNo][a][b][c];
-		}
-		if (b == 1) { // Разговор с однокласниками.
-			player.subject[a].Points += 2;
-			player.stat[stats.charisma].Points += 2;
-		}
-		if (b == 2) { // Попытался сбежать с урока.
-			lOE[dayNo][a][b][c];
-		}
-	}
-}
--- main.js
+++ main.js
@@ -433,21 +433,21 @@ window.onload = function() {
 	
 
 	const roof01 = new MapObject(4),
-		roof02 = new MapObject(4),
+		  roof02 = new MapObject(4),
+		  
+		  roof1 = new MapObject(5),
+		  roof2 = new MapObject(6),
+		  roof3 = new MapObject(7),
+		  roof31 = new MapObject(8),
+		  roof32 = new MapObject(8),
 
-		roof1 = new MapObject(5),
-		roof2 = new MapObject(6),
-		roof3 = new MapObject(7),
-		roof31 = new MapObject(8),
-		roof32 = new MapObject(8),
-
-		tree11 = new MapObject(1),
-		tree12 = new MapObject(1),
-		tree13 = new MapObject(1),
-		tree14 = new MapObject(1),
-		tree15 = new MapObject(1),
-
-		fountain = new MapObject(9);
+		  tree11 = new MapObject(1),
+		  tree12 = new MapObject(1),
+		  tree13 = new MapObject(1),
+		  tree14 = new MapObject(1),
+		  tree15 = new MapObject(1),
+		  
+		  fountain = new MapObject(9);
 
 	roof01.placeAt(1, 23, 1);
 	roof02.placeAt(36, 23, 1);
